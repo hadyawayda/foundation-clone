53d747a76db71df6d77a47026c6bcc9e007a684e
Hady <hady_awayda@hotmail.com>
2025-03-04 00:56:13 +0200

    Updated news section

--- components/News/MediaCard.tsx
+++ components/News/MediaCard.tsx
@@ -16,22 +16,22 @@ const MediaCard = ({
   animationClass = "",
 }: MediaCardProps) => {
   return (
-    <div className={`foundation-card w-full sm:w-1/3 px-4 ${animationClass}`}>
-      <div className="relative overflow-hidden">
+    <div className={`foundation-card w-full sm:w-1/3 px-6 ${animationClass}`}>
+      <div className="news-image-container relative overflow-hidden">
         <Image
           src={`/Assets/Images/${imageSrc}`}
           width={600}
           height={400}
           alt={title}
-          className="w-full h-auto object-cover"
+          className="h-full w-full object-cover"
         />
-        <div className="news-label absolute top-10 left-0 bg-white px-2 py-1 text-xs font-bold uppercase">
+        <div className="news-label absolute top-10 left-0 bg-white px-4 py-1 text-xs font-bold uppercase">
           {title}
         </div>
       </div>
-      <div className="mt-4">
-        <p className="text-red-600 font-bold text-sm uppercase">{date}</p>
-        <p className="text-gray-800 mt-2">{description}</p>
+      <div className="mt-2">
+        <p className="news-date font-black uppercase">{date}</p>
+        <p className="mt-2">{description}</p>
       </div>
     </div>
   );
