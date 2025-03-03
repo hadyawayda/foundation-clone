d98202805ad7db1e3e20abd8c8fd8cb20e6f6b25
Hady <hady_awayda@hotmail.com>
2025-03-03 02:53:02 +0200

    Updated carousel image and functionality

--- components/Foundations/DesktopFoundationsSection.tsx
+++ components/Foundations/DesktopFoundationsSection.tsx
@@ -4,14 +4,13 @@ import { images } from "../../constants/Foundation";
 
 const DesktopFoundationsSection = () => {
 	return (
-		<div className="hidden lg:flex w-full h-[45vw] group">
+		<div className="foundation-container hidden lg:flex w-full h-[45vw] group">
         {images.map((image, index) => (
           <Link
             key={index}
             href={image.href}
-            className="group/image relative flex-[1_1_33%] h-full transition-all duration-500 ease-in-out hover:flex-[1_1_70%] hover:siblings:flex-[1_1_15%]"
+            className="foundation-image-container relative flex-[1_1_33%] h-full transition-all duration-500 ease-in-out hover:flex-[1_1_70%] hover:siblings:flex-[1_1_15%]"
           >
-            {/* Image */}
             <Image
               className="object-cover w-full h-full"
               src={image.src}
@@ -20,18 +19,14 @@ const DesktopFoundationsSection = () => {
               alt={image.alt}
             />
 
-            {/* Gradient Overlay */}
             <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/40"></div>
 
-            {/* Label & Arrow Container (Only Hovered Image's Text Stays Visible) */}
             {image.label && image.href && (
-              <div className="absolute inset-0 flex items-end justify-between px-4 pb-8 transition-opacity duration-500 opacity-100 group-hover:opacity-0 group-hover/image:opacity-100 foundation-label-container">
-                {/* Label */}
-                <div className="text-white text-4xl whitespace-nowrap">
+              <div className="foundation-label-container absolute bottom-2 left-0 right-0 flex items-end justify-between px-4 py-6 transition-opacity duration-500 opacity-100 group-hover:opacity-0">
+                <div className="foundation-label text-white text-4xl whitespace-nowrap">
                   {image.label}
                 </div>
-                {/* Arrow (Animates on Hover) */}
-                <div className="relative transition-transform duration-500 ease-in-out group-hover/image:-translate-x-3 group-hover/image:-translate-y-3">
+                <div className="foundation-arrow right-4 xl:right-6 bottom-1.5 relative">
                   <Image
                     src="/Assets/Images/arrow-white.svg"
                     width={24}
--- components/Foundations/FoundationsSection.tsx
+++ components/Foundations/FoundationsSection.tsx
@@ -1,85 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
 import { images } from "../../constants/Foundation";
+import DesktopFoundationsSection from "./DesktopFoundationsSection";
 
 const FoundationsSection = () => {
   return (
     <div className="w-full -z-10">
-      <div className="foundation-container hidden lg:flex w-full h-[45vw] group">
-        {images.map((image, index) => (
-          <Link
-            key={index}
-            href={image.href}
-            className="foundation-image-container relative flex-[1_1_33%] h-full transition-all duration-500 ease-in-out hover:flex-[1_1_70%] hover:siblings:flex-[1_1_15%]"
-          >
-            <Image
-              className="object-cover w-full h-full"
-              src={image.src}
-              width={1024}
-              height={768}
-              alt={image.alt}
-            />
+      <DesktopFoundationsSection />
 
-            <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/40"></div>
-
-            {image.label && image.href && (
-              <div className="foundation-label-container absolute bottom-2 left-0 right-0 flex items-end justify-between px-4 py-6 transition-opacity duration-500 opacity-100 group-hover:opacity-0">
-                <div className="foundation-label text-white text-4xl whitespace-nowrap">
-                  {image.label}
-                </div>
-                <div className="foundation-arrow right-4 xl:right-6 bottom-1.5 relative">
-                  <Image
-                    src="/Assets/Images/arrow-white.svg"
-                    width={24}
-                    height={24}
-                    alt="Arrow Icon"
-                  />
-                </div>
-              </div>
-            )}
-          </Link>
-        ))}
-      </div>
-
-      {/* Mobile View - Carousel */}
-      <div className="lg:hidden relative w-full h-[65vw] overflow-hidden">
-        <div className="carousel flex w-[300%] transition-transform duration-500 ease-in-out">
+      {/* Mobile View - CSS Scroll-Snap Carousel */}
+      <div className="lg:hidden relative w-full h-[70vh] overflow-hidden">
+        <div className="carousel">
           {images.map((image, index) => (
-            <Link
-              key={index}
-              href={image.href}
-              className="carousel-item w-full flex-shrink-0 relative"
-            >
-              {/* Image */}
+            <Link key={index} href={image.href} className="carousel-item">
               <Image
-                className="object-cover w-full h-[65vw]"
+                className="object-cover"
                 src={image.src}
                 width={1024}
                 height={768}
                 alt={image.alt}
               />
-
-              {/* Gradient Overlay */}
               <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/40"></div>
 
-              {/* Label & Arrow */}
-              {image.label && (
-                <div className="absolute bottom-4 left-4 text-white text-lg font-bold">
-                  {image.label}
-                </div>
-              )}
-              <div className="absolute bottom-4 right-4 text-white">➚</div>
+              {/* Label */}
+              {image.label && image.href && (
+							<div className="foundation-label-container absolute bottom-2 left-6 right-0 flex items-end justify-between px-4 py-6 transition-opacity duration-500 opacity-100 group-hover:opacity-0">
+							  <div className="carousel-label text-white font-semibold text-4xl whitespace-nowrap">
+								{image.label}
+							  </div>
+							  <div className="foundation-arrow right-10 xl:right-6 bottom-1.5 relative">
+								<Image
+								className="carousel-arrow"
+								  src="/Assets/Images/arrow-white.svg"
+								  width={24}
+								  height={24}
+								  alt="Arrow Icon"
+								/>
+							  </div>
+							</div>
+						  )}
             </Link>
           ))}
         </div>
 
-        {/* Navigation Dots */}
-        <div className="absolute bottom-2 left-1/2 transform -translate-x-1/2 flex space-x-2">
+        {/* Navigation Dots (Positioned at the Top) */}
+        <div className="carousel-nav">
           {images.map((_, index) => (
-            <div
-              key={index}
-              className="dot w-3 h-1 bg-gray-400 rounded transition-all duration-500 ease-in-out"
-            ></div>
+            <a key={index} href={`#slide-${index}`} className="dot"></a>
           ))}
         </div>
       </div>
