fa64db8f1cc771a2cb48f8752de92f63bfcfa056
Hady <hady_awayda@hotmail.com>
2025-03-03 21:56:37 +0200

    Updated fillbar animation function and thickness

--- components/Navbar/FillBar/FillBar.tsx
+++ components/Navbar/FillBar/FillBar.tsx
@@ -4,24 +4,24 @@ import { useEffect } from "react";
 const FillBar = () => {
   useEffect(() => {
     const updateProgressBar = () => {
-      const scrollTop = window.scrollY;
-      const docHeight =
-        document.documentElement.scrollHeight - window.innerHeight;
-      const scrollPercentage = (scrollTop / docHeight) * 100;
-      document.querySelector(
+      const progressBar = document.querySelector(
         ".scroll-progress"
-      ).style.width = `${scrollPercentage}%`;
+      ) as HTMLElement | null;
+
+      if (progressBar) {
+        const scrollTop = window.scrollY;
+        const docHeight =
+          document.documentElement.scrollHeight - window.innerHeight;
+        const scrollPercentage = (scrollTop / docHeight) * 100;
+        progressBar.style.width = `${scrollPercentage}%`;
+      }
     };
 
     window.addEventListener("scroll", updateProgressBar);
     return () => window.removeEventListener("scroll", updateProgressBar);
   }, []);
 
-  return (
-    <>
-      <div className="scroll-progress"></div>
-    </>
-  );
+  return <div className="scroll-progress"></div>;
 };
 
 export default FillBar;
